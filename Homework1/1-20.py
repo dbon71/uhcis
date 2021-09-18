e5f0604af044dc2424e7b3d07cda5db96397810d
davidbonilla <davidbonilla601@gmail.com>
2021-09-17 21:16:59 -0500

    changed _____

--- Homework1/1-20.py
+++ Homework1/1-20.py
@@ -1,1 +1,12 @@
+#David Bonilla
+#CIS2348 Ratner
+#PSID:1913106
+user_num1 = int('Enter integer:', input())
+print(user_num1)
+print('You entered:', user_num1)
+print('And', user_num1, 'cubed is', user_num1 * user_num1)
+user_num2 = int('Enter another integer:', input())
+print(user_num2)
+print(user_num1 + user_num2, 'is 9')
+print(user_num1 * user_num2, 'is 20')
 
