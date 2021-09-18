82046c81c0dfb1de1a37d5ccc3ce9446f457a250
davidbonilla <davidbonilla601@gmail.com>
2021-09-17 23:07:45 -0500

    changed _____

--- Homework1/3-19.py
+++ Homework1/3-19.py
@@ -1,1 +1,16 @@
+#David Bonilla
+#CIS2348 Ratner
+#PSID:1913106
+import math
+height = int(input())
+width = int(input())
+area = height*width
+need = area/350
+print('Enter wall height (feet):')
+print('Enter wall width (feet):')
+print('Wall area:', area,'square feet' )
+print('Paint needed:','{:.2f}'.format(need),'gallons')
+print('Cans needed:', round(need),'can(s)')
 
+print('Choose a color to paint the wall:')
+print('Cost of purchasing',color,'paint:',price)
