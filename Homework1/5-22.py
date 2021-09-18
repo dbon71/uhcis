2819e3b3f6882e66bbc1994e0a5226e9dee65ac0
davidbonilla <davidbonilla601@gmail.com>
2021-09-17 23:58:31 -0500

    changed _____

--- Homework1/5-22.py
+++ Homework1/5-22.py
@@ -1,1 +1,33 @@
+#David Bonilla
+#CIS2348 Ratner
+#PSID:1913106
 
+'Oil change' == 35
+'Tire rotation' == 19
+'Car wash' == 7
+'Car wax' == 12
+
+print("Davy's auto shop services")
+for service, cost in services():
+    print(f'{service} -- ${cost}')
+
+first_serv = input('\nSelect first service:\n')
+second_serv = input('Select second service:\n')
+
+print("\nDavy's auto shop invoice\n")
+
+try:
+    first_serv_cost = services[first_serv]
+    print(f'Service 1: {first_serv}, ${first_serv_cost}')
+except:
+    print('Service 1: No service')
+    first_serv = 0
+try:
+    second_serv_cost = services[second_serv]
+    print(f'Service 2: {second_service}, ${second_serv_cost}')
+except:
+    print('Service 2: No service')
+    second_service_cost = 0
+
+total_cost = first_service_cost + second_service_cost
+print(f'\nTotal: ${total_cost}')
