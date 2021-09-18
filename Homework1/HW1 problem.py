a5dfc71ef879f95220c4f14ea9fb071c49e20ba0
davidbonilla <davidbonilla601@gmail.com>
2021-09-17 23:09:40 -0500

    changed _____

--- Homework1/HW1 problem.py
+++ Homework1/HW1 problem.py
@@ -1,8 +1,8 @@
 #David Bonilla
 #CIS2348 Ratner
 #PSID:1913106
-print('Current Day\n')
 #Current Code
+print('Current Day')
 current_month = int(input())
 current_day = int(input())
 current_year = int(input())
@@ -11,7 +11,7 @@ birth_month = int(input())
 birth_day = int(input())
 birth_year = int(input())
 age = int(current_year - birth_year)
-if birth_month == current_month and birth_day == current_day:
+if (birth_month == current_month) and (birth_day == current_day):
     print('Happy Birthday! You are', age, 'years old')
 else:
     print('Your are', age, 'years old')
