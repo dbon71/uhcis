75d19f72c3c008999319431c83207715e7a79b83
dbon71 <89566613+dbon71@users.noreply.github.com>
2021-10-14 01:55:36 -0500

    Add files via upload

--- Homework 2/9-10.py
+++ Homework 2/9-10.py
@@ -1,10 +1,12 @@
 #David Bonilla
 #CIS2348 Ratner
 #PSID:1913106
+#imports the files
 import csv
-
+#input of the words
 input1 = input()
 
+#method adds the words to the lsit to check
 with open(input1, 'r') as wordsfile:
     words_reader = csv.reader(wordsfile)
     for row in words_reader:
@@ -13,5 +15,6 @@ with open(input1, 'r') as wordsfile:
 no_duplicates_in_list = list(dict.fromkeys(list_of_words))
 listlength = len(no_duplicates_in_list)
 
+#this prints and counts the amount of duplicated words
 for i in range(listlength):
     print(no_duplicates_in_list[i], list_of_words.count(no_duplicates_in_list[i]))
