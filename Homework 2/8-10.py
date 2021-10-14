4c76a2993774b4477ba66ec7e10657fa84859671
dbon71 <89566613+dbon71@users.noreply.github.com>
2021-10-14 01:55:08 -0500

    Add files via upload

--- Homework 2/8-10.py
+++ Homework 2/8-10.py
@@ -1,12 +1,15 @@
 #David Bonilla
 #CIS2348 Ratner
 #PSID:1913106
+#input for the name
 name = input()
 reverseName = ""
 name = name.strip()
+#loop through the name to check for spaces
 for c in range(len(name)):
     if(name[c] != ''):
         reverseName = name[c]+reverseName
+#checks the character of name if the characters are the same
 if(name==reverseName):
         print(name,'is a palindrome')
 else:
