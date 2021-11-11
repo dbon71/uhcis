98329a26618df49d57a75788c4dcee15a76f268e
dbon71 <89566613+dbon71@users.noreply.github.com>
2021-11-10 20:31:29 -0600

    Add files via upload

--- Homework 3/11-18.py
+++ Homework 3/11-18.py
@@ -1,1 +1,12 @@
+#David Bonilla
+#CIS2348 Ratner
+#PSID:1913106
 
+#input the user number
+userInput = input()
+#list container to check for pos-neg numbers
+myList = [int(x) for x in userInput.split() if (int(x)>=0)]
+#sorts number from least to greatest
+myList.sort()
+#print list 
+[print(x, end=' ') for x in myList]
