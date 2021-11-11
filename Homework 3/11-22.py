d495b54820b5f9b4d5df492ec8c08dbefa19a0e1
dbon71 <89566613+dbon71@users.noreply.github.com>
2021-11-10 20:31:00 -0600

    Add files via upload

--- Homework 3/11-22.py
+++ Homework 3/11-22.py
@@ -1,1 +1,14 @@
+#David Bonilla
+#CIS2348 Ratner
+#PSID:1913106
 
+#input of words
+word = input().split()
+
+#loop to print words and count frequency
+for x in word:
+    count = 0
+    for y in word:
+        if x == y:
+            count += 1
+    print(x, count)
