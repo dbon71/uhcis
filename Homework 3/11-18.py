60eb39d01af318f8fe77258088629cd4d622e1ff
dbon71 <89566613+dbon71@users.noreply.github.com>
2021-11-10 22:37:31 -0600

    Add files via upload

--- Homework 3/11-18.py
+++ Homework 3/11-18.py
@@ -4,9 +4,12 @@
 
 #input the user number
 userInput = input()
+
 #list container to check for pos-neg numbers
 myList = [int(x) for x in userInput.split() if (int(x)>=0)]
+
 #sorts number from least to greatest
 myList.sort()
-#print list 
+
+#print list
 [print(x, end=' ') for x in myList]
