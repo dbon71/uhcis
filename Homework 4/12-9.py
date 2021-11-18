05a95109ce3e3be2485e0190f6b978495441e34f
dbon71 <89566613+dbon71@users.noreply.github.com>
2021-11-17 19:41:21 -0600

    Add files via upload

--- Homework 4/12-9.py
+++ Homework 4/12-9.py
@@ -5,7 +5,7 @@
 #input values for name and age
 s = input().split()
 firstName = s[0]
-last = []
+list = []
 
 #while loop to check age error
 while firstName != "-1":
@@ -15,12 +15,12 @@ while firstName != "-1":
         age=0
 
     #append to list
-    last.append([firstName,age])
+    list.append([firstName,age])
 
     #re-prompt input
     s=input().split()
     firstName = s[0]
 
 #for loop to print out organized data
-for name_age in last:
+for name_age in list:
      print('{} {}'.format(name_age[0], name_age[1]))
