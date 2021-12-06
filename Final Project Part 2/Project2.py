ea1106894812796373e84b6ff84828a8e87aa226
dbon71 <89566613+dbon71@users.noreply.github.com>
2021-12-06 02:39:24 -0600

    Add files via upload

--- Final Project Part 2/Project2.py
+++ Final Project Part 2/Project2.py
@@ -61,7 +61,16 @@ item4 = fullInventory[9] + fullInventory[10]+ fullInventory[11]
 item5 = fullInventory[12] + fullInventory[13]+ fullInventory[14]
 item6 = fullInventory[15] + fullInventory[16]+ fullInventory[17]
 item7 = fullInventory[18] + fullInventory[19]+ fullInventory[20]
-fullInventory = [item3, item2, item4, item7, item6, item1, item5]
+ 
+item1 = item1[0],item1[5],item1[6],item1[3],item1[1]
+item2 = item2[0],item2[5],item2[6],item2[3],item2[1]
+item3 = item3[0],item3[5],item3[6],item3[3],item3[1]
+item4 = item4[0],item4[5],item4[6],item4[3],item4[1]
+item5 = item5[0],item5[5],item5[6],item5[3],item5[1]
+item6 = item6[0],item6[5],item6[6],item6[1],item6[3],item6[7]
+item7 = item7[0],item7[5],item7[6],item7[1],item7[3]
+
+fullInventory = (item3, item2, item4, item7, item6, item1, item5)
 print(fullInventory)
 
 #list for all laptop items in inventory
@@ -69,24 +78,24 @@ laptopInventory = []
 
 #for loop to check full inventory for laptop items
 for x in fullInventory:
-    if x[6] == "laptop":
-        print(x)
+    if x[2] == "laptop":
+        print('Laptop inventory:', x)
 
 #list for all damaged items in inventory
 damagedInventory = []
 
+#for loop to check full inventory for past service items
+for x in fullInventory:
+    if x[4] == "5/27/2022":
+        print('Past service date:', x)
+
 #for loop to check full inventory damaged items
 for x in fullInventory:
     if x[0] == "7346234":
-        print(x)
+        print('Damaged inventory:', x)
 
 #list for all items past service date in inventory
 pastServiceDateInventory = []
-
-#for loop to check full inventory for past service items
-for x in fullInventory:
-    if x[3] == "5/27/2022":
-        print(x)
 
 #date time for the day and time program was executed
 x = datetime.datetime.now()
@@ -105,14 +114,14 @@ while manufacturer != "q":
     for x in fullInventory:
 
         #if and elif to check if item exists, doesn't exist, or suggesting an alternative
-        if x == manufacturer or x == item or x != "damaged" and x != "5/27/2022":
-            itemExist = fullInventory[x]
-            print("Your item is:", fullInventory[x] )
-        elif x != manufacturer or x != item:
-            print("No such item in inventory")
+        if manufacturer in fullInventory and item in fullInventory:
+            itemExist.append(fullInventory[x])
+            print("Your item is:", itemExist )
         elif x == "damaged" or x == "5/27/2022":
             print("You may also want to consider", itemExist)
-
+        elif manufacturer not in fullInventory or item not in fullInventory:
+            print("No such item in inventory")
+        
     #re-prompting the user with the option to quit
     manufacturer = str(input('Enter another manufacturer or press q to quit:'))
     item = str(input('Enter another item or press q to quit:'))
